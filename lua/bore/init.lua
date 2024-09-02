4f3f6bc0d383a6dc14e38183e51dc4c2a26eb864
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-09-02 20:09:10 +0100

    Formatting

--- lua/bore/init.lua
+++ lua/bore/init.lua
@@ -4,42 +4,48 @@ local M = {}
 
 -- https://github.com/ibhagwan/fzf-lua/blob/f7f54dd685cfdf5469a763d3a00392b9291e75f2/lua/fzf-lua/utils.lua#L372-L404
 function M.get_visual_selection()
-    -- this will exit visual mode
-    -- use 'gv' to reselect the text
-    local _, csrow, cscol, cerow, cecol
-    local mode = vim.fn.mode()
-    if mode == 'v' or mode == 'V' or mode == '' then
-      -- if we are in visual mode use the live position
-      _, csrow, cscol, _ = unpack(vim.fn.getpos("."))
-      _, cerow, cecol, _ = unpack(vim.fn.getpos("v"))
-      if mode == 'V' then
-        -- visual line doesn't provide columns
-        cscol, cecol = 0, 999
-      end
-      -- exit visual mode
-      vim.api.nvim_feedkeys(
-        vim.api.nvim_replace_termcodes("<Esc>",
-          true, false, true), 'n', true)
-    else
-      -- otherwise, use the last known visual position
-      _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
-      _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
-    end
-    -- swap vars if needed
-    if cerow < csrow then csrow, cerow = cerow, csrow end
-    if cecol < cscol then cscol, cecol = cecol, cscol end
-    local lines = vim.fn.getline(csrow, cerow)
-    local n = M.tbl_length(lines)
-    if n <= 0 then return '' end
-    lines[n] = string.sub(lines[n], 1, cecol)
-    lines[1] = string.sub(lines[1], cscol)
-    return table.concat(lines, "\n")
+	-- this will exit visual mode
+	-- use 'gv' to reselect the text
+	local _, csrow, cscol, cerow, cecol
+	local mode = vim.fn.mode()
+	if mode == "v" or mode == "V" or mode == "" then
+		-- if we are in visual mode use the live position
+		_, csrow, cscol, _ = unpack(vim.fn.getpos("."))
+		_, cerow, cecol, _ = unpack(vim.fn.getpos("v"))
+		if mode == "V" then
+			-- visual line doesn't provide columns
+			cscol, cecol = 0, 999
+		end
+		-- exit visual mode
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+	else
+		-- otherwise, use the last known visual position
+		_, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
+		_, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+	end
+	-- swap vars if needed
+	if cerow < csrow then
+		csrow, cerow = cerow, csrow
+	end
+	if cecol < cscol then
+		cscol, cecol = cecol, cscol
+	end
+	local lines = vim.fn.getline(csrow, cerow)
+	local n = M.tbl_length(lines)
+	if n <= 0 then
+		return ""
+	end
+	lines[n] = string.sub(lines[n], 1, cecol)
+	lines[1] = string.sub(lines[1], cscol)
+	return table.concat(lines, "\n")
 end
 
 function M.tbl_length(T)
-  local count = 0
-  for _ in pairs(T) do count = count + 1 end
-  return count
+	local count = 0
+	for _ in pairs(T) do
+		count = count + 1
+	end
+	return count
 end
 
 M.copy_selected = function()
@@ -83,15 +89,20 @@ M.setup = function(opts)
 		range = true,
 		desc = "Paste text from clipboard using bore",
 	})
-	
+
 	local use_default_keybindings = opts["use_default_keybindings"] ~= nil and opts["use_default_keybindings"] or false
 	if not use_default_keybindings then
-		-- Use custom keybindings 
-		vim.api.nvim_set_keymap('n', '<leader>y', ':BoreCopy<CR>', { noremap = true, silent = true })
-		vim.api.nvim_set_keymap('n', '<leader>p', ':BorePaste<CR>', { noremap = true, silent = true })
-		vim.api.nvim_set_keymap('v', '<leader>y', ':BoreCopy<CR>', { noremap = true, silent = true })
-		vim.api.nvim_set_keymap('n', '<leader>yy', ':lua require("bore").copy_current_line()<CR>', { noremap = true, silent = true })
-		vim.api.nvim_set_keymap('v', '<leader>p', ':BorePaste<CR>', { noremap = true, silent = true })
+		-- Use custom keybindings
+		vim.api.nvim_set_keymap("n", "<leader>y", ":BoreCopy<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<leader>p", ":BorePaste<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("v", "<leader>y", ":BoreCopy<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap(
+			"n",
+			"<leader>yy",
+			':lua require("bore").copy_current_line()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap("v", "<leader>p", ":BorePaste<CR>", { noremap = true, silent = true })
 		return
 	end
 
